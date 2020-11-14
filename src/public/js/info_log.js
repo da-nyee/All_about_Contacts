03252a7ee9d3beb70d694c1bbbf1ebdb78aebc2a
daeun <leede0418@likelion.org>
2020-11-15 05:39:45 +0900

    feat: Load record from local storage

--- src/public/js/info_log.js
+++ src/public/js/info_log.js
@@ -14,11 +14,7 @@ let contacts_number = "";
 let contacts_start = "";
 let contacts_end = "";
 
-function getCheck() {
-    
-}
-
-// 로컬 스토리지에 기록하기
+// 로컬 스토리지에 레코드 저장하기
 function saveRecord() {
     // 콘택트렌즈 제품
     contacts_product += conProduct[0].value;
@@ -29,7 +25,7 @@ function saveRecord() {
         }
     }
     // 시력교정 종류
-    optical_type += opt_type.value;
+    optical_type += optType[0].value;
     //콘택트렌즈 도수
     contacts_number += conNum[0].value;
     // 착용기간 시작
@@ -37,11 +33,49 @@ function saveRecord() {
     // 착용기간 종료
     contacts_end += conEnd[0].value;
 
-    // 로컬 스토리지 저장
+    // 레코드 저장
     localStorage.setItem("contacts_product", contacts_product);
     localStorage.setItem("contacts_type", contacts_type);
     localStorage.setItem("optical_type", optical_type);
     localStorage.setItem("contacts_number", contacts_number);
     localStorage.setItem("contacts_start", contacts_start);
     localStorage.setItem("contacts_end", contacts_end);
+}
+
+let conProductChk = document.getElementsByName("con_product_chk");
+let conTypeChk = document.getElementsByName("con_type_chk");
+let optTypeChk = document.getElementsByName("opt_type_chk");
+let conNumChk= document.getElementsByName("con_num_chk");
+let conStartChk = document.getElementsByName("con_start_chk");
+let conEndChk = document.getElementsByName("con_end_chk");
+
+let contacts_product_val;
+let contacts_type_val;
+let optical_type_val;
+let contacts_number_val;
+let contacts_start_val;
+let contacts_end_val;
+
+// 로컬 스토리지에서 레코드 로드하기
+function loadRecord() {
+    // 레코드 로드
+    contacts_product_val = localStorage.getItem("contacts_product");
+    contacts_type_val = localStorage.getItem("contacts_type");
+    optical_type_val = localStorage.getItem("optical_type");
+    contacts_number_val = localStorage.getItem("contacts_number");
+    contacts_start_val = localStorage.getItem("contacts_start");
+    contacts_end_val = localStorage.getItem("contacts_end");
+
+    // 콘택트렌즈 제품
+    conProductChk[0].value = contacts_product_val;
+    // 콘택트렌즈 종류
+    conTypeChk[0].value = contacts_type_val;
+    // 시력교정 종류
+    optTypeChk[0].value = optical_type_val;
+    // 콘택트렌즈 도수
+    conNumChk[0].value = contacts_number_val;
+    // 착용기간 시작
+    conStartChk[0].value = contacts_start_val;
+    // 착용기간 종료
+    conEndChk[0].value = contacts_end_val;
 }
