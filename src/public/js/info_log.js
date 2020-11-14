38b93a5c96b6c0ae38842d63fb6b2ed71205efe5
daeun <leede0418@likelion.org>
2020-11-15 03:34:24 +0900

    feat: Implement record

---
+++ src/public/js/info_log.js
@@ -0,0 +1,12 @@
+let localStorage = window.localStorage;     // 로컬 스토리지
+
+let conProduct = document.getElementById("con_product");    // 콘택트렌즈 제품
+let conType = document.getElementsByName("con_type");       // 콘택트렌즈 종류
+let optType = document.getElementById("opt_type");          // 시력교정 종류
+let conNum = document.getElementById("con_num");            // 콘택트렌즈 도수
+let conStart = document.getElementById("con_start");        // 착용기간 시작
+let conEnd = document.getElementById("con_end");            // 착용기간 종료
+
+function pageLoad() {
+
+}
